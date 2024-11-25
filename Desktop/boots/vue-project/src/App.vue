2ebea65168047dae7c68014dfd633cb058a35f1c
beka177 <bekaseitkali@gmail.com>
2024-11-25 15:11:38 +0500

    com1

---
+++ Desktop/boots/vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="text-center mt-4">Учет товаров</h1>
+        <form action="">
+          <div class="mb-3">
+            <label for="name" class="form-label">Название</label>
+            <input type="text" class="form-control" id="name">
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label">Дата добавления</label>
+            <input type="text" class="form-control" id="name">
+          </div>
+          <div class="d-grid gap-2 col-6 mx-auto">
+  <button class="btn btn-primary" type="button">Button</button>
+  <button class="btn btn-primary" type="button">Button</button>
+</div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+</template>
+
