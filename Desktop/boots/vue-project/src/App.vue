3516149505e22bcfe44dc6072b24211b8360250a
beka177 <bekaseitkali@gmail.com>
2024-11-25 15:59:24 +0500

    com2

--- Desktop/boots/vue-project/src/App.vue
+++ Desktop/boots/vue-project/src/App.vue
@@ -3,29 +3,49 @@
 </script>
 
 <template>
-<template>
-  <div class="container">
+    <div class="container">
     <div class="row">
-      <div class="col">
-        <h1 class="text-center mt-4">Учет товаров</h1>
-        <form action="">
-          <div class="mb-3">
-            <label for="name" class="form-label">Название</label>
-            <input type="text" class="form-control" id="name">
-          </div>
-          <div class="mb-3">
-            <label for="" class="form-label">Дата добавления</label>
-            <input type="text" class="form-control" id="name">
-          </div>
-          <div class="d-grid gap-2 col-6 mx-auto">
-  <button class="btn btn-primary" type="button">Button</button>
-  <button class="btn btn-primary" type="button">Button</button>
-</div>
-        </form>
-      </div>
+        
+        <div class="col">
+            <h1 class="text-center mt-4">Учёт товаров</h1>
+            <form action="">
+                <div class="mb-3">
+                    <label for="date" class="form-label">Название</label>
+                    <input type="text" class="form-control" id="name" >
+                </div>
+                <div class="mb-3">
+                    <label for="date" class="form-label">Дата</label>
+                    <input type="date" class="form-control" id="name" >
+                </div>
+                <div class="mb-3">
+                    <label for="date" class="form-label">Количество</label>
+                    <input type="number" class="form-control" id="name" >
+                </div>
+                <div class="mb-3">
+                    <label for="date" class="form-label">Цена</label>
+                    <input type="number" class="form-control" id="name" >
+                </div>
+                <div class="text-center mb-3">
+                    <button type="button" class="btn btn-dark">Добавить</button>
+                </div>
+            </form>
+        </div>
     </div>
+    <div class="card text-center">
+  <div class="card-header">
+    Featured
   </div>
-</template>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-body-secondary">
+    2 days ago
+  </div>
+</div>
+    
+</div>
 
 </template>
 
